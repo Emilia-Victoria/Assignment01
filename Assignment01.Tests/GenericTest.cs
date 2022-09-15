36755405d9e2367eb2585e4750d23887a1b0cd5f
Megabd <bath@itu.dk>
2022-09-15 09:33:11 +0200

    Now uses homemade classes to drive home the point better.

--- Assignment01.Tests/GenericTest.cs
+++ Assignment01.Tests/GenericTest.cs
@@ -15,13 +15,13 @@ public class GenericTest
         result.Equals(2);
     }
 
-    public void Return_2_Given_0_1_2_3_4_7_and_3_as_x_g2()
+    public void Return_2_Given_PersonWithAge_0_1_2_3_4_7_max_3()
     {
         // Arrange
-        int[] items2 = new int[] {0,1,2,3,4,7};
+        MyPersonWithAgeAndName[] items2 = new MyPersonWithAgeAndName[] {new MyPersonWithAgeAndName(0,"Steve"),new MyPersonWithAgeAndName(1,"Steven"),new MyPersonWithAgeAndName(2,"Steels"),new MyPersonWithAgeAndName(3,"Stacy"),new MyPersonWithAgeAndName(4,"Bill"),new MyPersonWithAgeAndName(7,"Laura")};
 
         // Act
-        int result2 = Generic.GreaterCount2<int,int>(items2, 3);
+        int result2 = Generic.GreaterCount2<MyPersonWithAgeAndName,MyPersonWithAge>(items2, new MyPersonWithAgeAndName(3,"Isa"));
 
         // Assert
         result2.Equals(2);
--- Assignment01/Generic.cs
+++ Assignment01/Generic.cs
@@ -44,18 +44,24 @@
 
 
     /* Explaination: 
-        The two classes, here referred to as GreaterCount and GreaterCount2, have a few differences, due to type constraints.
+The two classes, here referred to as GreaterCount and GreaterCount2, have a few differences, due to type constraints.
 First off, GreaterCount has only one type constraint, which is “where T : IComparable<T>”.
-This means that T, must either be or implement the interface IComparable, and moreover,
-given the <T> at the end, T must be able to use the compareTo() method on an object of the same type T.
+This means that type T, must either be or implement the interface IComparable<T>, where T is its own type. This meams it must have a
+compareTo() method, where it can be compared to other object of the same class. 
 
 For our method ”GreaterCount<T, U>(IEnumerable<T> items, T x)”, this means that the objects in our Enumerable list “items” must be of,
 and be able to be compared to, the given type T. Since our x must also be of, and be able to be compared to, the given type T,
 we can now safely implement the compareTo method in our execution, as shown above. The type U has no bearing on anything,
-as it is not used elsewhere in the method signature. Therefore, we can pass any object type, including the slim Object type,
-and even the new and slimmer Generic type, and as seen in the tests it changes nothing. 
+as it is not used elsewhere in the method signature. Therefore, we can pass any object type, including the slim Generic type. 
+And as seen in the tests it changes nothing. 
 
-In our GreaterCount2 method, we have two type constraints, which are “where T : U” and “where U : IComparable<U>”. First off, “where T : U” means that T must either be, or be a subclass of, type U. So whatever type U we give, T must be a subclass of it, or the same type. Second, “where U : IComparable<U>” means the same thing as “where T : IComparable<T>”, but for the type U of course. Since T must be of type, or be a subclass of , U, and subclasses must implement all interfaces of the parent, this all means that T must also implement IComparable<U>, and must be able to be compared to type U. In practice, this means that the given type T must be comparable to the given type U, which must either be the same type as T, or a parent of T. Therefore, T must be comparable to itself, and possibly any parent of T we might give as U. All in all, we can still safely use our compareTo method in our execution.
+In our GreaterCount2 method, we have two type constraints, which are “where T : U” and “where U : IComparable<U>”.
+First off, “where T : U” means that T must either be, or be a subclass of, type U. So whatever type U we give, T must be a subclass of it, or the same type.
+Second, “where U : IComparable<U>” means the same thing as “where T : IComparable<T>”, but for the type U of course.
+Since T must be of type, or be a subclass of, U, and subclasses must implement all interfaces of the parent,
+this all means that T must also implement IComparable<U>, and therefore be able to be compared to type U.
+In practice, this means that the given type T must be comparable to the given type U, which must either be the same type as T, or a parent of T.
+Therefore, T must be comparable to itself, and possibly any parent of T we might give as U. All in all, we can still safely use our compareTo method in our execution.
 
     
     */
